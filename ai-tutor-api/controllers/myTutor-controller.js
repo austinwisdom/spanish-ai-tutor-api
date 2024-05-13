efef06298c3115958bcdd782510c0dca022250ea
austinwisdom <austin.n.wisdom@gmail.com>
2024-05-13 17:26:44 -0300

    Added routes and controller functions for 6 languages.

--- ai-tutor-api/controllers/myTutor-controller.js
+++ ai-tutor-api/controllers/myTutor-controller.js
@@ -4,11 +4,92 @@ require("dotenv").config()
 const { OPENAI_API_KEY } = process.env
 const openai = new OpenAI({ apiKey: OPENAI_API_KEY});
 
-// Spanish tutor
-const myTutor = async (req, res) => {
+const tutorPrompts = [
+  {id: "es", tutorPrompt: "You are a male Spanish language teacher named Maxi. You answer any questions that your student asks you. Be helpful and friendly. Your student speaks English so take opportunities to explain key similarities and differences between Spanish and English as necessary or relevant."},
+  {id: "en", tutorPrompt: "You are a male English language teacher named David. You answer any questions that your student asks you. Be helpful and friendly. Your student speaks English so take opportunities to explain helpful information related to their question as necessary or relevant."},
+  {id: "fr", tutorPrompt: "You are a male French language teacher named Marvin. You answer any questions that your student asks you. Be helpful and friendly. Your student speaks English so take opportunities to explain key similarities and differences between French and English as necessary or relevant."},
+  {id: "de", tutorPrompt: "You are a female German language teacher named Anna. You answer any questions that your student asks you. Be helpful and friendly. Your student speaks English so take opportunities to explain key similarities and differences between German and English as necessary or relevant."},
+  {id: "jp", tutorPrompt: "You are a female Japanese language teacher named Yuki. You answer any questions that your student asks you. Be helpful and friendly. Your student speaks English so take opportunities to explain key similarities and differences between Spanish and English as necessary or relevant."},
+  {id: "pt", tutorPrompt: "You are a female Brazilian Portuguese language teacher named Isabela. You answer any questions that your student asks you. Be helpful and friendly. Your student speaks English so take opportunities to explain key similarities and differences between Brazilian Portuguese and English as necessary or relevant."},
+]
+
+// Spanish language tutor for lessons
+const mySpanishTutor = async (req, res) => {
 
   const message = req.body?.message
-  const prompt = "You are a Spanish speaker from Spain and work as a professional language coach. Assisting non-Spanish speakers to practice speaking Spanish, learn Spanish grammar and vocabulary, and to study Spanish."
+  const prompt = tutorPrompts[0]
+
+  try {
+    console.log(message)
+    const completion = await openai.chat.completions.create({
+      messages: [
+        {
+          role: "system",
+          content: prompt
+        },
+        { role: "user", content: message },
+      ],
+      model: "gpt-3.5-turbo-0125",
+      
+    });
+
+    if (completion.choices[0].message.content) {
+      console.log(completion.choices[0].message.content);
+      const answer = completion.choices[0].message.content
+      res.json({
+          message: answer
+      })
+      } else {
+          res.status(404).json("Unable to get AI res")
+      }
+
+  } catch {
+    console.error(error)
+  }
+  
+}
+
+// English language tutor for lessons
+const myEnglishTutor = async (req, res) => {
+
+  const message = req.body?.message
+  const prompt = tutorPrompts[1]
+
+  try {
+    console.log(message)
+    const completion = await openai.chat.completions.create({
+      messages: [
+        {
+          role: "system",
+          content: prompt
+        },
+        { role: "user", content: message },
+      ],
+      model: "gpt-3.5-turbo-0125",
+      
+    });
+
+    if (completion.choices[0].message.content) {
+      console.log(completion.choices[0].message.content);
+      const answer = completion.choices[0].message.content
+      res.json({
+          message: answer
+      })
+      } else {
+          res.status(404).json("Unable to get AI res")
+      }
+
+  } catch {
+    console.error(error)
+  }
+  
+}
+
+// French language tutor for lessons
+const myFrenchTutor = async (req, res) => {
+
+  const message = req.body?.message
+  const prompt = tutorPrompts[2]
 
   try {
     console.log(message)
@@ -44,7 +125,79 @@ const myTutor = async (req, res) => {
 const myGermanTutor = async (req, res) => {
 
   const message = req.body?.message
-  const prompt = "You are a German language teacher. You answer any questions that your student asks you. Be helpful and friendly. Your student speaks English so take opportunities to explain key similarities and differences between German and English as necessary or relevant."
+  const prompt = "You are a female German language teacher named Anna. You answer any questions that your student asks you. Be helpful and friendly. Your student speaks English so take opportunities to explain key similarities and differences between German and English as necessary or relevant."
+
+  try {
+    console.log(message)
+    const completion = await openai.chat.completions.create({
+      messages: [
+        {
+          role: "system",
+          content: prompt
+        },
+        { role: "user", content: message },
+      ],
+      model: "gpt-3.5-turbo-0125",
+      
+    });
+
+    if (completion.choices[0].message.content) {
+      console.log(completion.choices[0].message.content);
+      const answer = completion.choices[0].message.content
+      res.json({
+          message: answer
+      })
+      } else {
+          res.status(404).json("Unable to get AI res")
+      }
+
+  } catch {
+    console.error(error)
+  }
+  
+}
+
+// Japanese language tutor for lessons
+const myJapaneseTutor = async (req, res) => {
+
+  const message = req.body?.message
+  const prompt = tutorPrompts[4]
+
+  try {
+    console.log(message)
+    const completion = await openai.chat.completions.create({
+      messages: [
+        {
+          role: "system",
+          content: prompt
+        },
+        { role: "user", content: message },
+      ],
+      model: "gpt-3.5-turbo-0125",
+      
+    });
+
+    if (completion.choices[0].message.content) {
+      console.log(completion.choices[0].message.content);
+      const answer = completion.choices[0].message.content
+      res.json({
+          message: answer
+      })
+      } else {
+          res.status(404).json("Unable to get AI res")
+      }
+
+  } catch {
+    console.error(error)
+  }
+  
+}
+
+// Portuguese language tutor for lessons
+const myPortugueseTutor = async (req, res) => {
+
+  const message = req.body?.message
+  const prompt = tutorPrompts[5]
 
   try {
     console.log(message)
@@ -77,5 +230,5 @@ const myGermanTutor = async (req, res) => {
 }
 
 module.exports = {
-    myTutor, myGermanTutor
+  mySpanishTutor, myGermanTutor, myEnglishTutor, myFrenchTutor, myJapaneseTutor, myPortugueseTutor
 }
--- ai-tutor-api/routes/myTutor.js
+++ ai-tutor-api/routes/myTutor.js
@@ -4,7 +4,11 @@ const  axios  =  require('axios');
 
 const myTutorController = require("../controllers/myTutor-controller")
 
-router.route("/").post(myTutorController.myTutor)
-router.route("/german").post(myTutorController.myGermanTutor)
+router.route("/es").post(myTutorController.mySpanishTutor)
+router.route("/en").post(myTutorController.myEnglishTutor)
+router.route("/de").post(myTutorController.myGermanTutor)
+router.route("/fr").post(myTutorController.myFrenchTutor)
+router.route("/jp").post(myTutorController.myJapaneseTutor)
+router.route("/pt").post(myTutorController.myPortugueseTutor)
 
 module.exports = router
